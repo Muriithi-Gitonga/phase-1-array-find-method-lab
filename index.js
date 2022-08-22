d82c524b54f9a27461b262772b3651fa4497d6c0
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-08-22 22:43:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 //code your solution here
 
-// function superbowlWin(array) {
-//     for (let win of array){
-//         if (win.result === "W") {
-//             return win.year
-//         }
-//     }
-// }
 
 function superbowlWin(array) {
     const found = array.find(item => item.result ==='W')
@@ -15,6 +8,8 @@ function superbowlWin(array) {
     }
     
 }
+
+
 
 
 
